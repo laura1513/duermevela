a5439ce2372fe54f986652f8a23d7949e3eac386
laura1513 <laurajorques2003@gmail.com>
2024-03-27 19:12:11 +0100

    Página de inicio de sesión y registro creada correctamente

---
+++ script/datos.js
@@ -0,0 +1,31 @@
+function validarRegistro() {
+    var usuario = document.getElementById("user").value;
+    var email = document.getElementById("email").value;
+    var contrasena = document.getElementById("password").value;
+    var confirmarContrasena = document.getElementById("confPassword").value;
+    var errorMensaje = document.getElementById("errorMensaje");
+
+    errorMensaje.innerHTML = "";
+
+    if (usuario === "" || email === "" || contrasena === "" || confirmarContrasena === "") {
+        errorMensaje.innerHTML = "Por favor complete todos los campos.";
+        return false;
+    }
+
+    if (contrasena !== confirmarContrasena) {
+        errorMensaje.innerHTML = "Las contraseñas no coinciden.";
+        Notification.requestPermission()
+            .then(permission => {
+                if (permission === "granted") {
+                    new Notification("Las contraseñas no coinciden");
+                }
+            });
+        return false;
+    }
+
+    return true;
+}
+function cerrarAnuncio() {
+    var anuncio = document.getElementById("anuncio");
+    anuncio.innerHTML = "";
+}
